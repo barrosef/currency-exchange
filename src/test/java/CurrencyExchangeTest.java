1197ec31149ffecef732a6e93eeafb035c0827a6
barrosef <barrosef@gmail.com>
2022-03-13 01:47:15 -0300

    Setup (skeleton) add initial resource and test just start only.

--- src/test/java/CurrencyExchangeTest.java
+++ src/test/java/CurrencyExchangeTest.java
@@ -1,20 +1,9 @@
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
 
-import javax.ws.rs.core.Response;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
-@QuarkusTest
+ @QuarkusTest
 public class CurrencyExchangeTest {
 
-    @Test
     public void testConvertEndpoint() {
-        given()
-                .when().get("/convert")
-                .then()
-                .statusCode(is(Response.Status.ACCEPTED))
-                ;
+
     }
 }
