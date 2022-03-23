8d9f7a237f8b27482e342319064c81482f1c383c
barrosef <barrosef@gmail.com>
2022-03-23 01:00:38 -0300

    Skeleton (api) added Resource api

--- src/main/java/challenge/currencyexchange/resources/CurrencyExchangeResource.java
+++ src/main/java/challenge/currencyexchange/resources/CurrencyExchangeResource.java
@@ -1,5 +1,6 @@
 package challenge.currencyexchange.resources;
 
+import challenge.currencyexchange.service.ExchangeRateService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -7,6 +8,8 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -21,14 +24,17 @@ import static javax.ws.rs.core.Response.Status.CREATED;
 @Tag(name = "convertion", description = "Currency convertion")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class CurrencyExchange {
+public class CurrencyExchangeResource {
+
+    @Inject
+    ExchangeRateService service;
 
     @POST
     @Operation(summary = "To convert currency",
             description = "Convert from EUR from any currency")
     @APIResponses(value = @APIResponse(responseCode = "201", description = "Currency conversion success",
             content = @Content(mediaType = "application/json")))
-    public Response convert() {
+    public Response doConvert(@Valid ExchangeRateRequest exchangeRateRequest) {
         return Response.ok().status(CREATED).build();
     }
 
