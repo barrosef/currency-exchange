ffcf577147843832c42865fdd3347081aa5c221f
barrosef <barrosef@gmail.com>
2022-03-23 01:36:21 -0300

    Skeleton (api) included api HeaderParam user_login

--- src/main/java/challenge/currencyexchange/resources/CurrencyExchangeResource.java
+++ src/main/java/challenge/currencyexchange/resources/CurrencyExchangeResource.java
@@ -3,6 +3,7 @@ package challenge.currencyexchange.resources;
 import challenge.currencyexchange.service.ExchangeRateService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
@@ -10,10 +11,7 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -25,6 +23,9 @@ import static javax.ws.rs.core.Response.Status.CREATED;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class CurrencyExchangeResource {
+
+    @HeaderParam("user_login")
+    String user;
 
     @Inject
     ExchangeRateService service;
