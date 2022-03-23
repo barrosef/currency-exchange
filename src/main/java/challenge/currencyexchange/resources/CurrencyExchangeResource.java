5010f40944dcaa33be99f9c2f190d0aa53c0a695
barrosef <barrosef@gmail.com>
2022-03-23 01:56:33 -0300

    Skeleton (resource) included GET to list conversions

--- src/main/java/challenge/currencyexchange/resources/CurrencyExchangeResource.java
+++ src/main/java/challenge/currencyexchange/resources/CurrencyExchangeResource.java
@@ -3,7 +3,6 @@ package challenge.currencyexchange.resources;
 import challenge.currencyexchange.service.ExchangeRateService;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
@@ -15,6 +14,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.Response.Status.ACCEPTED;
 import static javax.ws.rs.core.Response.Status.CREATED;
 
 @ApplicationScoped
@@ -35,8 +35,22 @@ public class CurrencyExchangeResource {
             description = "Convert from EUR from any currency")
     @APIResponses(value = @APIResponse(responseCode = "201", description = "Currency conversion success",
             content = @Content(mediaType = "application/json")))
-    public Response doConvert(@Valid ExchangeRateRequest exchangeRateRequest) {
-        return Response.ok().status(CREATED).build();
+    public Response postToConvert(@Valid ExchangeRateRequest exchangeRateRequest) {
+        return Response
+                .ok(service.rate(exchangeRateRequest))
+                .status(CREATED)
+                .build();
     }
 
+    @GET
+    @Operation(summary = "To list user conversions",
+            description = "List conversions")
+    @APIResponses(value = @APIResponse(responseCode = "200", description = "List user conversions success",
+            content = @Content(mediaType = "application/json")))
+    public Response getExchangeList() {
+        return Response
+                .ok(service.list(this.user))
+                .status(ACCEPTED)
+                .build();
+    }
 }
