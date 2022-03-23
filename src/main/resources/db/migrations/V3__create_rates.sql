9ee45eb1b8021f085ee337ed47b49767c303c36e
barrosef <barrosef@gmail.com>
2022-03-22 22:34:54 -0300

    Setup (database) added rate and rate_detail tables

---
+++ src/main/resources/db/migrations/V3__create_rates.sql
@@ -0,0 +1,22 @@
+create table rate_cextb01 (
+                              rate_id bigint not null,
+                              base varchar(255),
+                              base_value double,
+                              conversion_rate double,
+                              dateTime timestamp,
+                              user_id integer,
+                              primary key (rate_id)
+);
+
+create table rate_detail_cextb02 (
+                             rate_detail_id bigint not null,
+                             currency_value double not null,
+                             conversion_tax double not null,
+                             currency_name varchar(255) not null,
+                             primary key (rate_detail_id, currency_name)
+);
+
+alter table rate_detail_cextb02
+    add constraint FKg4q9a131wpa78cg8wsw1kougo
+        foreign key (rate_detail_id)
+            references rate_cextb01;
