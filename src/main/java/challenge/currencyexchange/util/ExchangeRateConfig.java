fb65907258d005300baa0e82eb66361ee2469145
barrosef <barrosef@gmail.com>
2022-03-24 00:49:47 -0300

    Setup (configuration) included conversionTax

--- src/main/java/challenge/currencyexchange/util/ExchangeRateConfig.java
+++ src/main/java/challenge/currencyexchange/util/ExchangeRateConfig.java
@@ -2,15 +2,14 @@ package challenge.currencyexchange.util;
 
 import io.smallrye.config.ConfigMapping;
 
-import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
-@ApplicationScoped
 @ConfigMapping(prefix = "challenge.currencyexchange")
 public interface ExchangeRateConfig {
 
     String apiAccessKey();
     String rootUserLogin();
     List<String> permittedCoins();
+    Double conversionTax();
 
 }
