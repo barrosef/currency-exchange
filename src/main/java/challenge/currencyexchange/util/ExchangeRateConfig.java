1d8f468df0039f1e8d22114efeb567813d01d291
barrosef <barrosef@gmail.com>
2022-03-23 22:16:10 -0300

    Setup (configuration) included permitted coins

--- src/main/java/challenge/currencyexchange/util/ExchangeRateConfig.java
+++ src/main/java/challenge/currencyexchange/util/ExchangeRateConfig.java
@@ -2,9 +2,15 @@ package challenge.currencyexchange.util;
 
 import io.smallrye.config.ConfigMapping;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
 @ConfigMapping(prefix = "challenge.currencyexchange")
 public interface ExchangeRateConfig {
 
     String apiAccessKey();
     String rootUserLogin();
+    List<String> permittedCoins();
+
 }
